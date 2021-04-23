f67a05e074e37ac9e9f2dc007b69e6819a913b14
j-tomasik <jackedwardtomasik@gmail.com>
2021-04-23 13:30:49 -0700

    Added emojis
    Changed name and emojis to better reflect owner

--- cNk3ZvTr/index.js
+++ cNk3ZvTr/index.js
@@ -1,4 +1,4 @@
-const myEmojis = ["👨‍💻", "⛷", "🍲"]
+const myEmojis = ["😎","🏄‍♂️","💸","🏀","🏊‍♂️","🥇","🏋️",]
 
 function renderEmojis() {
     const emojiContainer = document.getElementById("emoji-container")
